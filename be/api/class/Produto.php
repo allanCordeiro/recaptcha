ab472817c8d32ad90344e87c95031d6388555f8b
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2020-10-12 17:06:49 -0300

    API para trazer lista de produtos

---
+++ be/api/class/Produto.php
@@ -0,0 +1,29 @@
+<?php
+
+class Produto
+{
+    public function mostrar()
+    {
+        $con = new PDO('mysql: host=localhost; dbname=base_teste;',
+            'teste',
+            '123@456'
+        );
+
+        $sql = "SELECT id, name FROM produtos";
+        $sql = $con->prepare($sql);
+        $sql->execute();
+
+        $resultados = array();
+
+        while($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+            $resultados[] = $row;
+        }
+
+        if(!$resultados) {
+            throw new Exception("Nenhum produto no estoque!");
+        }
+
+        return $resultados;
+
+    }
+}
